430495692714b7890123753fa0466d2aa91c8fea
Yerbolat <yerbolat.assabay@gmail.com>
2024-03-10 22:42:44 +0500

    dostyq

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Post, Gallery, Friend, FriendRequest, Comment, ReplyComment
+
+
+class GalleryAdminTab(admin.TabularInline):
+    model = Gallery
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [GalleryAdminTab]
+    list_editable = ['active']
+    list_display = ['thumbnail', 'user', 'title', 'visibility', 'active']
+    prepopulated_fields = {"slug": ('title',)}
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_editable = ['active']
+    list_display = ['thumbnail', 'post', 'active']
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_editable = ['status']
+    list_display = ['sender', 'receiver', 'status']
+
+
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ['user', 'friend']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    # inlines = [ReplyCommentTabAdmin]
+    list_display = ['user', 'post', 'comment', 'active']
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    # inlines = [ReplyCommentTabAdmin]
+    list_display = ['user', 'comment', 'active']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(Friend, FriendAdmin)
+admin.site.register(FriendRequest, FriendRequestAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(ReplyComment, ReplyAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,147 @@
 from django.db import models
+from django.utils.html import mark_safe
+from django.utils.text import slugify
+from userauths.models import User, Profile, user_directory_path
 
-# Create your models here.
+
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
+
+VISIBILITY = (
+    ("pending", "pending"),
+    ("accept", "accept"),
+    ("reject", "reject"),
+)
+
+FRIEND_REQUEST = (
+    ("Only Me", "Only Me"),
+    ("Everyone", "Everyone"),
+)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    image = models.ImageField(
+        upload_to=user_directory_path, blank=True, null=True)
+    video = models.FileField(
+        upload_to=user_directory_path, blank=True, null=True)
+    visibility = models.CharField(
+        max_length=100, choices=VISIBILITY, default="Everyone")
+    pid = ShortUUIDField(length=7, max_length=25,
+                         alphabet='abcdefghijklmnopqrstuvwxyz')
+    likes = models.ManyToManyField(User, blank=True, related_name="like")
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+    views = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        else:
+            return self.user.username
+
+    def save(self, *args, **kwargs):
+        uuid_key = shortuuid.uuid()
+        uniqueid = uuid_key[:2]
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + "-" + uniqueid
+
+        super(Post, self).save(*args, **kwargs)
+
+    def thumbnail(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />' % (self.image))
+
+
+class Gallery(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="gallery", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __srt__(self):
+        return str(self.post)
+
+    class Meta:
+        verbose_name_plural = 'Gallery'
+
+    def thumbnail(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />' % (self.image))
+
+
+class FriendRequest(models.Model):
+    fid = ShortUUIDField(length=7, max_length=25,
+                         alphabet='abcdefghijklmnopqrstuvwxyz')
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="receiver")
+    status = models.CharField(
+        max_length=100, default="pending", choices=FRIEND_REQUEST)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __srt__(self):
+        return str(self.sender)
+
+    class Meta:
+        verbose_name_plural = 'FriendRequest'
+
+
+class Friend(models.Model):
+    fid = ShortUUIDField(length=7, max_length=25,
+                         alphabet='abcdefghijklmnopqrstuvwxyz')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user")
+    friend = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="friend")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __srt__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name_plural = 'Friend'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comment_user")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=1000)
+    active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, blank=True, related_name="comment_likes")
+    cid = ShortUUIDField(length=7, max_length=25,
+                         alphabet='abcdefghijklmnopqrstuvwxyz')
+
+    def __srt__(self):
+        return str(self.post)
+
+    class Meta:
+        verbose_name_plural = 'Comment'
+
+
+class ReplyComment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reply_user")
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    reply = models.CharField(max_length=1000)
+    active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    cid = ShortUUIDField(length=7, max_length=25,
+                         alphabet='abcdefghijklmnopqrstuvwxyz')
+
+    def __srt__(self):
+        return str(self.comment)
+
+    class Meta:
+        verbose_name_plural = 'Reply Comment'
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notification_user")
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notification_sender")
